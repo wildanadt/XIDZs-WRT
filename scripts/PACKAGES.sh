ffa40e11cef7e362cbcdb967818f84903e879e39
de quenx <fidzxidz@gmail.com>
2025-10-04 00:17:00 +0700

    Update PACKAGES.sh

--- scripts/PACKAGES.sh
+++ scripts/PACKAGES.sh
@@ -64,7 +64,7 @@ packages_custom+=(
     
     "luci-app-tinyfm_|https://api.github.com/repos/bobbyunknown/luci-app-tinyfm/releases/latest"
     "luci-app-droidnet_|https://api.github.com/repos/animegasan/luci-app-droidmodem/releases/latest"
-    "luci-theme-alpha_|https://api.github.com/repos/derisamedia/luci-theme-alpha/releases/latest"
+    "luci-theme-alpha4_|https://api.github.com/repos/derisamedia/luci-theme-alpha/releases/latest"
     "luci-app-tailscale_|https://api.github.com/repos/asvow/luci-app-tailscale/releases/latest"
     #"luci-app-rakitanmanager_|https://api.github.com/repos/rtaserver/RakitanManager/releases/latest"
     "luci-app-ipinfo_|https://api.github.com/repos/bobbyunknown/luci-app-ipinfo/releases/latest"
